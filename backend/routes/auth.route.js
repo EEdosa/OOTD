7a99be0f2c90cc3aa50630eb57a961f9afe23029
Nebi <nxm8324@mavs.uta.edu>
2025-10-05 04:54:34 -0500

    Auth

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { signup, login, logout } from '../controllers/auth.controller.js';
+import { signup, login, logout, refreshToken } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
 router.post("/signup", signup);  // If user clicks the signup button, call the signup function.
 router.post("/login", login);    // If user clicks the login button, call the login function.
 router.post("/Logout", logout);  // If user clicks the logout button, call the logout function.
+router.post("/refresh-token", refreshToken);
 
 export default router;
